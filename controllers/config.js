21a5dca3a10ef0e3ef58d87713d047ad179baef0
Cbrito03 <63610450+Cbrito03@users.noreply.github.com>
2020-09-01 23:41:07 -0500

    Update config.js

--- controllers/config.js
+++ controllers/config.js
@@ -6,10 +6,14 @@ var info =
 };
 
 var horario_WA = {
-  "OPEN_HOUR" : 8,
+  /*"OPEN_HOUR" : 8,
   "OPEN_MINUTE" : 0,
-  "CLOSE_HOUR" : 23, // 21
-  "CLOSE_MINUTE" : 0,
+  "CLOSE_HOUR" : 21,
+  "CLOSE_MINUTE" : 0,*/
+  "OPEN_HOUR" : 1,
+  "OPEN_MINUTE" : 0,
+  "CLOSE_HOUR" : 23,
+  "CLOSE_MINUTE" : 59,
   dias : {
     "0" : ["domingo",true],
     "1" : ["lunes",true],
@@ -22,10 +26,14 @@ var horario_WA = {
 };
 
 var horario_FB = {
-  "OPEN_HOUR" : 7,
+ /* "OPEN_HOUR" : 7,
   "OPEN_MINUTE" : 0,
-  "CLOSE_HOUR" : 23,//22
-  "CLOSE_MINUTE" : 0,
+  "CLOSE_HOUR" : 22,
+  "CLOSE_MINUTE" : 0,*/
+  "OPEN_HOUR" : 1,
+  "OPEN_MINUTE" : 0,
+  "CLOSE_HOUR" : 23,
+  "CLOSE_MINUTE" : 59,
   dias : {
     "0" : ["domingo",true],
     "1" : ["lunes",true],
@@ -38,10 +46,14 @@ var horario_FB = {
 };
 
 var horario_TW = {
-  "OPEN_HOUR" : 7,
+  /*"OPEN_HOUR" : 7,
   "OPEN_MINUTE" : 0,
-  "CLOSE_HOUR" : 23,//22
-  "CLOSE_MINUTE" : 0,
+  "CLOSE_HOUR" : 23,
+  "CLOSE_MINUTE" : 0,*/
+  "OPEN_HOUR" : 1,
+  "OPEN_MINUTE" : 0,
+  "CLOSE_HOUR" : 23,
+  "CLOSE_MINUTE" : 59,
   dias : {
     "0" : ["domingo",true],
     "1" : ["lunes",true],
